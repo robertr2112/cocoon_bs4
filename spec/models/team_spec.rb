49bdaf1b48175a750a32d12256aa534e26ef1e4e
Robert Reynolds <robertr2112@yahoo.com>
2021-02-01 14:00:14 -0600

    Started getting RSpec testing working

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -14,19 +14,14 @@ require 'rails_helper'
 RSpec.describe Team, type: :model do
 
   before(:each) do
-    @Team= Team.create(Name: "Chicago Bears" , awayTeamIndex: 1,
-                              spread: 3.5)
+    @Team= Team.create(name: "Chicago Bears" , nfl: 1)
   end
 
-  subject {@game}
+  subject {@Team}
 
-  it { should respond_to(:week_id) }
-  it { should respond_to(:homeTeamIndex) }
-  it { should respond_to(:awayTeamIndex) }
-  it { should respond_to(:spread) }
-  it { should respond_to(:homeTeamScore) }
-  it { should respond_to(:awayTeamScore) }
-  it { should respond_to(:wonGame?) }
+  it { should respond_to(:name) }
+  it { should respond_to(:nfl) }
+  it { should respond_to(:imagePath) }
 
   it { should be_valid }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,8 +65,6 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  config.include AuthenticationHelper
-  config.include ApplicationHelper
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     Rails.application.load_seed # loading seeds
